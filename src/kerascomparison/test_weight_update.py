Corrected batch gradient descent: divide by number of samples in batch

src/kerascomparison/test_weight_update.py
@@ -10,7 +10,7 @@ model = Sequential()
 model.add(Dense(45, input_dim = 7, activation="tanh", kernel_initializer=init, bias_initializer=init))
 model.add(Dense(6, activation="linear", kernel_initializer=init, bias_initializer=init))
 
-sgd = optimizers.SGD(lr=0.1,momentum=0.0, decay=0, nesterov=False)
+sgd = optimizers.SGD(lr=0.1,momentum=0.9, decay=0, nesterov=True)
 rmsprop = optimizers.rmsprop(lr=0.001, rho=0.9, epsilon=1e-8);
 
 model.compile(loss="mean_squared_error", optimizer=sgd)
@@ -223,7 +223,7 @@ from convert_keras_weights import convert, printConverted
 model.set_weights(weights)
 model.fit(np.array([
       [1,2,3,4,5,6,7],
-   #  [1,2,3,4,5,5,7],
+       [1,2,3,4,5,5,7],
    #  [1,2,3,4,5,4,7], 
    #   [1,2,3,4,5,3,7], 
    #   [1,2,3,4,5,2,7], 
@@ -233,35 +233,6 @@ model.fit(np.array([
   ]),
   np.array([
         [1,2,3,4,5,6],
-    #    [1,2,3,4,5,6], 
-     #   [1,2,3,4,5,6], 
-       # [1,2,3,4,5,6], 
-      #  [1,2,3,4,5,6], 
-     #   [1,2,3,4,5,6], 
-     #   [1,2,3,4,5,6], 
-     #   [1,2,3,4,5,6]
-        ]), epochs=1, batch_size=1)
-
-np.set_printoptions(suppress=True)
-diff = np.array(model.get_weights()) - np.array(weights);
-printConverted(convert(diff))
-
-print("--------------------------------")
-print("--------------------------------")
-print("--------------------------------")
-
-model.fit(np.array([
-    #  [1,2,3,4,5,6,7],
-     [1,2,3,4,5,5,7],
-   #  [1,2,3,4,5,4,7], 
-   #   [1,2,3,4,5,3,7], 
-   #   [1,2,3,4,5,2,7], 
-   #   [1,2,3,4,5,1,7], 
-   #   [1,2,3,4,5,6,7], 
-   #   [1,2,3,4,5,6,7]
-  ]),
-  np.array([
-      #  [1,2,3,4,5,6],
         [1,2,3,4,5,6], 
      #   [1,2,3,4,5,6], 
        # [1,2,3,4,5,6], 

src/kerascomparison/test_weight_update_simple.py
@@ -23,7 +23,7 @@ model.set_weights(weights)
 prediction = model.predict(np.array([[1,1]]))
 print("Predicted: " + str(prediction))
 
-model.fit(np.array([[1,1]]), np.array([[0.5, 0.25]]), epochs=1, batch_size=1)
+model.fit(np.array([[1,1], [2, 2]]), np.array([[0.5, 0.25], [0.75, 0.375]]), epochs=1, batch_size=2)
 np.set_printoptions(suppress=True)
 modelWeights = model.get_weights()
 diff = []
@@ -36,16 +36,16 @@ printConverted(convert(diff))
 prediction = model.predict(np.array([[1,1]]))
 print("Predicted: " + str(prediction))
 
-model.fit(np.array([[2,2]]), np.array([[0.75, 0.375]]), epochs=1, batch_size=1)
-modelWeights = model.get_weights()
-diff = []
-for i in range(len(modelWeights)):
-    diff.append(modelWeights[i] - betweenWeights[i]);
-printConverted(convert(diff))
+#model.fit(np.array([[2,2]]), np.array([[0.75, 0.375]]), epochs=1, batch_size=1)
+#modelWeights = model.get_weights()
+#diff = []
+#for i in range(len(modelWeights)):
+#    diff.append(modelWeights[i] - betweenWeights[i]);
+#printConverted(convert(diff))
 
 
 
-prediction = model.predict(np.array([[1,1]]))
-print("Predicted: " + str(prediction))
+#prediction = model.predict(np.array([[1,1]]))
+#print("Predicted: " + str(prediction))
 #print(model.get_updates_for(np.array([[1,1,1,1,1,1,1]])))
 