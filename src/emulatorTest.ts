bcef44f4f44f4b21e947d19416faf4af6d36b2a6
Tino Fuhrmann <tino-fuhrmann@web.de>
2018-07-02 20:26:46 +0200

    Removed old files

--- src/emulatorTest.ts
+++
@@ -1,7 +0,0 @@
-import {Point} from './math'
-import {CarEmulator, Car} from './model/car'
-let car = new Car(new Point(5, 5), 0);
-let emulator = new CarEmulator(car)
-import {Vector} from './neuralnet/math'
-emulator.forward(new Vector([5, 5, 0, 45/180*Math.PI]));
-console.log(emulator.backward(new Vector([3, 3, 45/180 * Math.PI])));
