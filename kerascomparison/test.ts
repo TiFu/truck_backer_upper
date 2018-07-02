41566f02c3bddf5204ee4e6767f92701d7798c8f
Tino Fuhrmann <tino-fuhrmann@web.de>
2018-07-02 20:53:18 +0200

    Removed python samples

--- kerascomparison/test.ts
+++
@@ -1,8 +0,0 @@
-import {WeightedMSE} from '../neuralnet/error';
-import {Vector} from '../neuralnet/math'
-
-let mse = new WeightedMSE(new Vector([1, 2, 3]))
-
-console.log(mse.getError(new Vector([1.25,2.5, 4]), new Vector([1, 2, 3])))
-
-console.log(mse.getErrorDerivative(new Vector([1.25,2.5, 4]), new Vector([1, 2, 3])))
--- python/saveWeights.py
+++
@@ -1,8 +0,0 @@
-from keras.models import load_model
-
-model = load_model("./model2")
-
-for layer in model.layers:
-    weights = layer.get_weights()
-    print(weights)
-    print("")
